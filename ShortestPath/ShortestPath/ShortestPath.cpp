dfbaa278013ac814bcd82713fe5affd71b1677cf
evasp-00 <121692985+SpoonWasAlreadyTaken@users.noreply.github.com>
2024-12-17 18:01:51 +0200

    wroks

--- ShortestPath/ShortestPath/ShortestPath.cpp
+++ ShortestPath/ShortestPath/ShortestPath.cpp
@@ -3,6 +3,8 @@
 #include <algorithm>
 #include <math.h>
 #include <string>
+#include <thread>
+#include <windows.h>
 
 using namespace std;
 
@@ -25,37 +27,42 @@ int main()
 	vector<int> startIndex;
 	int endIndex;
 	cout << "Type in width of the grid: \n";
-	cin >> sizeX;
-	cout << "Type in height of the grid: \n";
 	cin >> sizeY;
+	cout << "Type in height of the grid: \n";
+	cin >> sizeX;
 
-	for (int i = 0; i < sizeX; i++)
+	for (int i = 0; i < sizeY; i++)
 	{
 		grid.push_back({});
 		costGrid.push_back({});
-		for (int j = 0; j < sizeY; j++)
+		for (int j = 0; j < sizeX; j++)
 		{
 			grid.at(i).push_back('#');
-			costGrid.at(i).push_back(RandomNumber(0, 10));
+			costGrid.at(i).push_back(RandomNumber(0, 9));
 		}
 	}
 
 	while (true)
 	{
 		int input;
-
-		cout << "Input index of start position, type -1 for random\n";
-		cin >> input;
-		if (input > sizeX * sizeY - 1)
+		cout << "Input number of start positions\n";
+		int repeats;
+		cin >> repeats;
+		for (int i = 0; i < repeats; i++)
 		{
-			input = sizeX * sizeY - 1;
-		}
+			cout << "Input index of start position, type -1 for random\n";
+			cin >> input;
+			if (input > sizeX * sizeY - 1)
+			{
+				input = sizeX * sizeY - 1;
+			}
 
-		if (input < 0)
-		{
-			input = RandomNumber(0, sizeX * sizeY - 1);
+			if (input < 0)
+			{
+				input = RandomNumber(0, sizeX * sizeY - 1);
+			}
+			startIndex.push_back(input);
 		}
-		startIndex.push_back(input);
 
 		cout << "Input index of end position, type -1 for random\n";
 		cin >> endIndex;
@@ -102,12 +109,6 @@ string ShortestPath(vector<int> startPositions, const int endPosition)
 	{
 		for (int i = 0; i < cPos.size(); i++)
 		{
-			cout << "posX: " << cPos.at(i).at(0) << " posY: " << cPos.at(i).at(1) << "\n";
-		}
-		
-
-		for (int i = 0; i < cPos.size(); i++)
-		{
 			grid.at(cPos.at(i).at(0)).at(cPos.at(i).at(1)) = '+';
 		}
 
@@ -116,9 +117,9 @@ string ShortestPath(vector<int> startPositions, const int endPosition)
 			int moveX = 0;
 			int moveY = 0;
 
-			if (cPos.at(i).at(0) != GetX(endPosition))
+			if (cPos.at(i).at(1) != GetX(endPosition))
 			{
-				if (cPos.at(i).at(0) < GetX(endPosition))
+				if (cPos.at(i).at(1) < GetX(endPosition))
 				{
 					moveX = 1;
 				}
@@ -127,9 +128,9 @@ string ShortestPath(vector<int> startPositions, const int endPosition)
 					moveX = -1;
 				}
 			}
-			if (cPos.at(i).at(1) != GetY(endPosition))
+			if (cPos.at(i).at(0) != GetY(endPosition))
 			{
-				if (cPos.at(i).at(1) < GetY(endPosition))
+				if (cPos.at(i).at(0) < GetY(endPosition))
 				{
 					moveY = 1;
 				}
@@ -142,7 +143,7 @@ string ShortestPath(vector<int> startPositions, const int endPosition)
 			
 			if (moveX != 0 && moveY != 0)
 			{
-				if (costGrid.at(cPos.at(i).at(1)).at(cPos.at(i).at(0) + moveX) == costGrid.at(cPos.at(i).at(0)).at(cPos.at(i).at(1) + moveY))
+				if (costGrid.at(cPos.at(i).at(0)).at(cPos.at(i).at(1) + moveX) == costGrid.at(cPos.at(i).at(1)).at(cPos.at(i).at(0) + moveY))
 				{
 					if (abs(GetX(endPosition) - cPos.at(i).at(0)) < abs(GetY(endPosition) - cPos.at(i).at(1)))
 					{
@@ -155,7 +156,7 @@ string ShortestPath(vector<int> startPositions, const int endPosition)
 				}
 				else
 				{
-					if (costGrid.at(cPos.at(i).at(1)).at(cPos.at(i).at(0) + moveX) > costGrid.at(cPos.at(i).at(0)).at(cPos.at(i).at(1) + moveY))
+					if (costGrid.at(cPos.at(i).at(0)).at(cPos.at(i).at(1) + moveX) > costGrid.at(cPos.at(i).at(1)).at(cPos.at(i).at(0) + moveY))
 					{
 						moveX = 0;
 					}
@@ -166,36 +167,52 @@ string ShortestPath(vector<int> startPositions, const int endPosition)
 				}
 			}
 			
-			cPos.at(i).at(0) += moveX;
-			cPos.at(i).at(1) += moveY;
+			cPos.at(i).at(1) += moveX;
+			cPos.at(i).at(0) += moveY;
 		}
-
+		
 		reachedEnd = true;
 		for (int i = 0; i < cPos.size(); i++)
 		{
-			if (cPos.at(i).at(0) != GetX(endPosition) && cPos.at(i).at(1) != GetY(endPosition))
+			if (cPos.at(i).at(0) != GetY(endPosition) || cPos.at(i).at(1) != GetX(endPosition))
 			{
 				reachedEnd = false;
 				break;
 			}
 		}
+		
+		output = "";
+
+		for (int i = 0; i < sizeY; i++)
+		{
+			for (int j = 0; j < sizeX; j++)
+			{
+				output += grid.at(i).at(j);
+				output += " ";
+			}
+			output += "\n";
+		}
+		system("CLS");
+		cout << output;
+		Sleep(50);
 	}
 
+	output = "";
 	for (int i = 0; i < cPos.size(); i++)
 	{
 		grid.at(cPos.at(i).at(0)).at(cPos.at(i).at(1)) = '+';
 	}
 
-	for (int i = 0; i < sizeX; i++)
+	for (int i = 0; i < sizeY; i++)
 	{
-		for (int j = 0; j < sizeY; j++)
+		for (int j = 0; j < sizeX; j++)
 		{
 			output += grid.at(i).at(j);
 			output += " ";
 		}
 		output += "\n";
 	}
-
+	system("CLS");
 	return output;
 }
 
